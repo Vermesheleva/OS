c47f994737730bc77c79039e79e6f5b622ab8e04
Vermesheleva <90042021+Vermesheleva@users.noreply.github.com>
2022-06-24 16:03:30 +0300

    Add files via upload

--- Lab 2 Создание потоков + тесты/Main.cpp
+++ Lab 2 Создание потоков + тесты/Main.cpp
@@ -1,28 +1,50 @@
-#include <windows.h>
+#include <boost\thread\thread.hpp>
+#include <boost\chrono\chrono.hpp>
 #include <iostream>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
-int sleepTime = 15;
 
-DWORD WINAPI polynom(LPVOID lparam) {
-	int* arr = (int*)lparam;
-	int deg = (int)arr[1];
-	int x = (int)arr[2];
+int sleepTime = 15;
+int* param1 = new int[10];
+int* param2 = new int[10];
+
+void sleep() {
+	boost::this_thread::sleep_for(boost::chrono::milliseconds(sleepTime));
+}
+
+void numerator() {
+	int deg = param1[1];
+	int x = param1[2];
 	int result = 0;
 
 	for (int i = 3; i < deg + 4; i++) {
-		int temp = arr[i] * pow(x, (i - 3));
-		Sleep(sleepTime);
+		int temp = param1[i] * pow(x, (i - 3));
+		sleep();
 		result += temp;
 	}
 
 	cout << "p(" << x << ") = " << result << endl;
-	arr[0] = result;
+	param1[0] = result;
 
-	return 0;
+}
+
+void denumerator() {
+	int deg = param2[1];
+	int x = param2[2];
+	int result = 0;
+
+	for (int i = 3; i < deg + 4; i++) {
+		int temp = param2[i] * pow(x, (i - 3));
+		sleep();
+		result += temp;
+	}
+
+	cout << "p(" << x << ") = " << result << endl;
+	param2[0] = result;
+
 }
 
 int* createData(string name, int x) {
@@ -54,37 +76,19 @@ int* createData(string name, int x) {
 	return param;
 }
 
-HANDLE* runThreads(int* param1, int* param2) {
-	HANDLE* handles = new HANDLE[2];
-	DWORD ID;
-	handles[0] = CreateThread(NULL, 0, polynom, (void*)param1, 0, &ID);
-	handles[1] = CreateThread(NULL, 0, polynom, (void*)param2, 0, &ID);
-
-	for(int i = 0; i < 2; i++)
-	if (handles[i] == NULL) {
-		cout << "The thread is not created." << endl;
-		CloseHandle(handles[1]);
-		CloseHandle(handles[0]);
-		delete[] param1;
-		delete[] param2;
-		exit(0);
-	}
-
-	return handles;
-}
 
 double calculateResult(int* param1, int* param2) {
 	double result = 0;
-	HANDLE* handles = new HANDLE[2];
-	handles = runThreads(param1, param2);
+	
+	boost::thread num(numerator);
+	boost::thread denum(denumerator);
 
-	WaitForMultipleObjects(2, handles, TRUE, INFINITE);
-	CloseHandle(handles[0]);
-	CloseHandle(handles[1]);
+	num.join();
+	denum.join();
 
 	if ((double)param2[0] == 0) {
 		cout << "The denomerator is 0." << endl;
-		//Ð´Ð»Ñ Ñ‚ÐµÑÑ‚Ð¾Ð²
+		//äëÿ òåñòîâ
 		return INFINITY;
 	}
 
@@ -102,7 +106,7 @@ int main() {
 
 	int* param1 = createData("numerator", x);
 	int* param2 = createData("denomerator", x);
-	
+
 	result = calculateResult(param1, param2);
 
 	if (result == INFINITY) {
@@ -110,7 +114,7 @@ int main() {
 	}
 
 	cout << "f(" << x << ") = " << result << endl;
-	
+
 	delete[] param1;
 	delete[] param2;
 	return 0;
